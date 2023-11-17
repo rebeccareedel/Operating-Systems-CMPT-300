45f83ce8216bcde9e87a5244467fb552286d102f
Asmita Srivastava <asmita_srivastava@sfu.ca>
2023-11-16 16:38:30 -0800

    added changes to files

--- Assignment 3/my_malloc.c
+++ Assignment 3/my_malloc.c
@@ -3,11 +3,81 @@
 #include "my_malloc.h"
 #include <stdlib.h>
 
+#define MAX_SIZE 256
+
+// each node in hashtable has key and value, 
+// key = number of bytes from base ptr, value = number of bytes allocated in segment
+typedef struct {
+    int key;
+    int value;
+} bytes;
+
+bytes * hash_table[MAX_SIZE];
+
+
+int num_free_bytes = 0; // used to check if compaction is necessary
+int first_call = 1; // used to check if first call to program and if should initialize DS
+int notInUseFront = 0;
+int notInUseBack = 1;
+
+void hash_table_insert(bytes *b) {
+    int index = b->key;
+    hash_table[index] = b->value;
+}
+// creates initial memory block, hashtable for showing current memory segments, 2 arrays (inUse & notInUse) for organization
+void mem_init(){
+    // get initial memory block -- 256 bytes
+    int *ptr = (int*)malloc(32 * sizeof(int));
+  
+    // create inUse Array -- should be empty but initialized to be size of 256 bytes
+    int * inUse[MAX_SIZE] = {NULL};
+
+    // create notInUse Array -- should be initialized to have 0 index
+    int * notInUse[MAX_SIZE] = {NULL};
+    notInUse[0] = 0; // 0 since currently only 1 big block with 256 bytes
+
+    // create and initialize hashtable for each 256 byte
+    bytes init = {.key = 0, .value = 256};
+    hash_table_insert(&init);
+}
+
+void print_table(){
+    for (int i=0; i< MAX_SIZE; i++){
+        if(hash_table[i] == NULL) {
+            printf("\t%i\t--\n", i);
+        } else{
+            printf("\t%i\t%i\n", i, hash_table[i]->value);
+        }
+    }
+}
+
+// returns pointer to memory of requested size, adds index to inUse array
 void *my_malloc(size_t size){
+    // check if first time calling program -- if yes, initialize values
+    if(first_call == 1){
+        mem_init();
+        first_call = 0;
+    }
+    print_table();
+
+    // get first index of notInUse Array -- check in hashtable if enough size -> repeat until finds spot
     return 0;
 }
 
 
+// makes memory available -- adds to notInUse array, checks if can coalesce with adjacent free memory
 void my_free(void *ptr){
+    // calculate key for hashtable
+    
+    // remove from inUse Array
+    
+    // look up size (value) by the key in hashtable 
+
+    // check if adjacent blocks in notInUse array by doing key+size and key-1 -- combine if yes
+
+    // update lower address to have both sizes and set bigger index size to 0
+    
+    // remove bigger index from notInUse Array
+    
     return 0;
 }

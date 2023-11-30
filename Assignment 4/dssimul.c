e4187e5d636e0d22a9a1bb84338199a19b8463d1
Asmita Srivastava <asmita_srivastava@sfu.ca>
2023-11-30 14:05:21 -0800

    Updated files

--- Assignment 4/dssimul.c
+++ Assignment 4/dssimul.c
@@ -2,5 +2,104 @@
 
 #include "dssimul.h"
 #include <stdlib.h>
-#include <stdio.h>  
+#include <stdio.h> 
+#include <assert.h> 
 
+#define n 50
+#define range 199
+
+// Shortest Seek Time First -- chooses request next closest to head -> not fair 
+void sstf(int arr){
+
+}
+
+// Scan -- heads keeps moving back and forth across disk -> prevents starvation, middle passed 2x more than end
+void scan(int arr){
+
+}
+
+void createRandomList(int **result_list){
+    // create list to hold values
+    *result_list = (int*)malloc(n * sizeof(int));
+    int in, im;
+
+    im = 0;
+
+    for (in = 0; in < range && im < n; ++in) {
+    int rn = range - in;
+    int rm = n - im;
+    if (rand() % rn < rm)    
+        /* Take it */
+        result_list[im++] = &in; /* +1 since your range begins from 1 */
+    }
+    assert(im == n);
+}
+
+void convertToList(char* str, int* num_elem, int **result_list){
+
+    // should have atleast 1 element
+    *num_elem = 1; 
+
+    // allocate space to hold result
+    *result_list = (int*)malloc(*num_elem * sizeof(int));
+    
+    // loop to split by comma
+    int currentIndex = 0; // necessary so we can skip over commas
+    int nextNumber = 0;  // holds next int to add to list, used to create ints from 1 digit to 3
+    
+    for (char* current = str; *current != '\0'; current++) {
+        if (*current == ',') // current = deliminter
+        {   
+            // make sure integer input in [0,199] range
+            if(nextNumber <0 || nextNumber > 199){
+                fprintf(stderr,"Please provide integers in [0,199] range \n");
+                exit(1);
+            }
+            (*result_list)[currentIndex++] = nextNumber; // add 
+            *num_elem += 1;
+            *result_list = realloc(*result_list, *num_elem * sizeof(int)); // allocate more space
+            nextNumber = 0;  // set to 0 for next value
+        } else {
+            // converts from string to int, using ASCII manipulation
+            nextNumber = nextNumber * 10 + (*current - '0'); 
+        }
+    }
+    // make sure integer input in [0,199] range
+    if(nextNumber <0 || nextNumber > 199){
+        fprintf(stderr,"Please provide integers in [0,199] range \n");
+        exit(1);
+    }
+    // add the last int
+    (*result_list)[currentIndex] = nextNumber;
+
+    // check number of elements, if <3 call make random
+    if(*num_elem <3){
+        // ERROR
+        fprintf(stderr,"Please provide atleast 3 unique tracks within the range [0,199]: \n");
+        exit(1);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int num_elem;
+    int* current_list;
+    
+    if(argc > 1){
+        // split by comma, pass as int, put each in list
+        convertToList(argv[1], &num_elem, &current_list);
+        for( int i = 0; i < num_elem; i ++){
+            printf("%d\n", current_list[i]);
+        }
+        
+    }
+    else{
+        // make list of 50 random integers
+        createRandomList(&current_list);
+        num_elem = n;
+        for( int i = 0; i < num_elem; i ++){
+            printf("%d\n", current_list[i]);
+        }
+    }
+    free(current_list);
+    return 0;
+}

d7b9c83324872cfd858621d44bcc1c2c6fc6e915
rmr6 <rmr6@sfu.ca>
2023-12-03 12:47:23 -0800

    Added to header and output files

--- Assignment 4/dssimul.h
+++ Assignment 4/dssimul.h
@@ -4,5 +4,16 @@
 #define _DSSIMUL_H_
 #include <stdlib.h>
 
+// Shortest Seek Time First -- chooses request next closest to head -> not fair 
+void sstf(int* arr, int num_elem);
+
+// Scan -- heads keeps moving back and forth across disk -> prevents starvation, middle passed 2x more than end
+void scan(int* arr, int num_elem);
+
+// used to create 50 int length list -> when the user doesn't give any input
+void createRandomList(int **result_list);
+
+// takes input from command line and makes it into a list to be used by the algorithms
+void convertToList(char* str, int* num_elem, int **result_list);
 
 #endif
